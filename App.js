241ebdf7d405d09759994216bf098a7d1d8e3f82
B00165618 Artem Mochuliak <B00165618@mytudublin.ie>
2025-09-29 15:06:14 +0100

    step 3

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import React, { useState } from 'react';
 import {StyleSheet,  Text, View } from 'react-native';
 
 
 
 export default function App() {
+  const [fullname, setFullname] = useState("Artem Mochuliak");
   return (
     <View>
-      <Text style={styles.paragraph}>Hello, World</Text>
+      <Text style={styles.paragraph}>Hello, World {fullname}</Text>
     </View>
   );
 }
